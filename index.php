d3d6b4f30c06a2aa83e0d155f99bec7a6f517c5b
mikkel500 <mikkellarsen500@gmail.com>
2014-02-18 10:31:53 +0100

    oprettede index filen.
    nu kan man vælge hvad der skal stå på sin forside

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include 'header.php'; ?>
+<div class="contentbox">velkommen til din side (ret teksten her til det ønskede)</div>
+<div class="contentbox">siden åbner snart!</div>
+<?php include 'footer.php'; ?>
