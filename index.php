464a2aa9c85a7516c29c56df2d021687db428d79
mikkel500 <mikkellarsen500@gmail.com>
2014-02-20 11:50:12 +0100

    session_start index
    tilføjet session_start og andre småting

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+session_start();
+// store session data
+$_SESSION['funktion']=ret-forside;
+?>
+
+<html>
+denne funktion kommer snart!
+<body>
+
+<?php
+//retrieve session data
+echo "funktion=". $_SESSION['funktioner'];
+?>
+
+</body>
+</html> 
 <?php include 'header.php'; ?>
 <div class="contentbox">velkommen til din side (ret teksten her til det ønskede)</div>
 <div class="contentbox">siden åbner snart!</div>
