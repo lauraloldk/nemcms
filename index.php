e312d0250d7bf156965d2fd76e1684d825b173f7
mikkel500 <mikkellarsen500@gmail.com>
2015-10-29 11:38:04 +0100

    fjernede mislykedes funktioner

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<?php
-session_start();
-// store session data
-$_SESSION['funktion']=ret-forside;
-?>
-
-<html>
-denne funktion kommer snart!
-<body>
-
-<?php
-//retrieve session data
-echo "funktion=". $_SESSION['funktioner'];
-?>
-
-</body>
-</html> 
 <?php include 'header.php'; ?>
 <div class="contentbox">velkommen til din side (ret teksten her til det ønskede)</div>
 <div class="contentbox">siden åbner snart!</div>
