559ae3712f9e604199fa2be09ff02ae7a0ff9c56
mikkel500 <mikkellarsen500@gmail.com>
2015-10-29 11:36:38 +0100

    Tilføjet direkte download af ncms
    Nu kan folk downloade nemcms fra din egen side.

--- index.php
+++ index.php
@@ -18,4 +18,6 @@ echo "funktion=". $_SESSION['funktioner'];
 <?php include 'header.php'; ?>
 <div class="contentbox">velkommen til din side (ret teksten her til det ønskede)</div>
 <div class="contentbox">siden åbner snart!</div>
+<div class="contentbox">Din hjemmeside bruger nemcms.</div>
+<div class="contentbox">Kan downloades her: https://github.com/mikkel500/nemcms/archive/master.zip</div>
 <?php include 'footer.php'; ?>
