85a1fdfa3e9a29d13eea71784f508da7ecf95c4e
mikkel500 <mikkellarsen500@gmail.com>
2015-10-30 14:30:51 +0100

    Create index.php

---
+++ test/modules/nyheder/index.php
@@ -0,0 +1,4 @@
+viser Alle nyheder (antal-nyheder)
+Hvis(antal-nyheder)=+10 så vis (knap:næste side)
+Nyheds databasefil = nemcms/test/modules/nyheder/nyheder.mkl
+hvis adminip.mkl indeholder din ip. så (vis adminpanel for nyheder)
