19395ceafd0a43dffc5abe7e2dd5631b63f62c93
mikkel500 <mikkellarsen500@gmail.com>
2015-10-31 19:24:15 +0100

    Delete index.php

--- test/modules/nyheder/index.php
+++
@@ -1,4 +0,0 @@
-viser Alle nyheder (antal-nyheder)
-Hvis(antal-nyheder)=+10 så vis (knap:næste side)
-Nyheds databasefil = nemcms/test/modules/nyheder/nyheder.mkl
-hvis adminip.mkl indeholder din ip. så (vis adminpanel for nyheder)
