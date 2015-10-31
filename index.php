7453cebd7ff091a7c1bfa1038efc622001526b67
mikkel500 <mikkellarsen500@gmail.com>
2015-10-31 19:22:52 +0100

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php include 'header.php'; ?>
-<div class="contentbox">velkommen til din side (ret teksten her til det ønskede)</div>
-<div class="contentbox">siden åbner snart!</div>
-<div class="contentbox">Din hjemmeside bruger nemcms.</div>
-<div class="contentbox">Kan downloades her: https://github.com/mikkel500/nemcms/archive/master.zip</div>
-<?php include 'footer.php'; ?>
